421b0e0050466f1795cd2e8e113895d3ca737f8c
YuriBrasiliano <yuribrasiliano37@gmail.com>
2022-10-19 00:54:31 -0300

    Finalizando o cadastro de novos produtos

--- app/src/main/java/com/example/spacechat/MarketPlaceActivity.java
+++ app/src/main/java/com/example/spacechat/MarketPlaceActivity.java
@@ -5,15 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
+
+
 public class MarketPlaceActivity extends AppCompatActivity {
     private BottomNavigationView mns;
+    private Button novoProduto;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_market_place);
         mns = findViewById(R.id.bottom_navigation3);
+        novoProduto = findViewById(R.id.btnnovoProduto);
         mns.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -24,6 +31,12 @@ public class MarketPlaceActivity extends AppCompatActivity {
                         startActivity(new Intent(MarketPlaceActivity.this, Perfil.class));break;
                 }
                 return true;
+            }
+        });
+        novoProduto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MarketPlaceActivity.this, CadastroProduto.class));
             }
         });
     }
